b28057d3aaae1a4fa3808b03f6aad3c006b9a86d
lewis marx <lowrck@gmail.com>
2017-02-18 15:19:31 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var tmdbclient = require('themoviedbclient');
+var tmdb = new tmdbclient('de4833475e61d855b5e77ecf00273f96');
+
+
+
+/* retrieve latest movies from DB */
+router.get("/Movies", function(req, res, err) {
+
+    tmdb.call("/movie/now_playing", {})
+        .then(function (data) {
+
+
+            var moviedata;
+            for(var i = 0; i < data.results.length; i++) {
+
+                moviedata += "  <div class='row'> <div class='col-md-6'> <img alt='Movie Image' src='https://image.tmdb.org/t/p/w300"+ data.results[i].backdrop_path + "' class='img-rounded'> <h2>" + data.results[i].original_title + "</h2> <p>" + data.results[i].overview + "</p> </div> <div class='col-md-6'> </div> </div>"
+
+
+            }
+
+
+
+
+
+
+
+
+            res.render('movies', {moviedata : moviedata.substring(11)});
+        });
+
+})
+
+
+
+
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+module.exports = router;
